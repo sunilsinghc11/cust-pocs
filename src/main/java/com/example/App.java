89b4f3c31bf4cbf7c7440086ed676fd32c69999f
sunilsinghc11 <166667153+sunilsinghc11@users.noreply.github.com>
2024-09-16 12:10:52 -0400

    Update App.java

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -2,9 +2,7 @@ package com.example;
 
 public class App {
     public static void main(String[] args) {
-        String password = "gho_16C7e42F292c6912E7710c838347Ae178B4a";
         System.out.println("Welcome to MGM DEMO");
-        System.out.println("Your password is: " + password);
     }
 }
 
