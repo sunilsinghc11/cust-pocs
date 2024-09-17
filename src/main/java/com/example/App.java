32cb653a903e515aae9eff1dda12d40c65f0f100
sunilsinghc11 <sunils@jfrog.com>
2024-09-17 10:00:08 -0400

    New Property

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -2,7 +2,8 @@ package com.example;
 
 public class App {
     public static void main(String[] args) {
-        String password = "gho_16C7e42F292c6912E7710c838347Ae178B4a";
+        String password1 = "gho_16C7e42F292c6912E7710c838347Ae178B4a";
+        String password = "fdsfdsfsdfsqfsdfdfqs1237"
         System.out.println("Welcome to MGM DEMO");
         System.out.println("Your password is: " + password);
     }
